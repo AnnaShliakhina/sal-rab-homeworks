12c4b94bb0a20373466aaba13c055db8a8cf1f9b
ShlyakhinaAV <gavrik24av@yandex.ru>
2021-10-17 17:53:26 +0300

    выполнение задания 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,10 +6,14 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
-
+let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-
+if(productsSum == 0 || productsSum >= freeShippingMinSum){
+    shippingSum = 0;
+    } else if(productsSum > 0 || productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice
+    }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
 
